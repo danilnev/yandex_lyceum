1ce4200e3ce344b5dce4d4c5b4f1fce10a8e43e0
danilnev <danilanevinsky@gmail.com>
2023-02-09 20:55:53 +0300

    Make full additional task 2 in lesson 26 (lambda-functions)

--- Lesson 26. Lambda-functions/additional_2_astroid.py
+++ Lesson 26. Lambda-functions/additional_2_astroid.py
@@ -1,17 +1,17 @@
 import math
 
 R = 1
-min_s = None
-point = (0, 75)
+min_s = 100
+point = (0.75, 0)
 numbers = []
 i = 0
 while i <= math.pi * 2:
     numbers.append(i)
-    i += 0.01
+    i += 1 * (10 ** -4)
 for t in numbers:
-    x = R * math.cos(t)
-    y = R * math.sin(t)
+    x = R * (math.cos(t) ** 3)
+    y = R * (math.sin(t) ** 3)
     s = math.sqrt((x - point[0]) ** 2 + (y - point[1]) ** 2)
-    if not min_s or s < min_s:
+    if s < min_s:
         min_s = s
-print(min_s)
+print(round(min_s, 4))
